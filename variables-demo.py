48f6ac24bd1ce241250949e10e2c6aa567747edc
BURAK OZDEMIR <ozdemirburak1912@gmail.com>
2022-10-24 14:48:32 +0300

    variables

---
+++ variables-demo.py
@@ -0,0 +1,35 @@
+
+# 1- Bir müşterinin aşağıdaki bilgileri için değişken oluşturunuz
+
+
+    # Müşteri adı
+    # Müşteri soyadı
+    # Müşteri ad+soyad
+    # Müşteri Cinsiyet
+    # Müşteri tc kimlik
+    # müşteri doğum yılı
+    # müşteri adres bilgisi
+    # müşteri yaşı
+
+musteriAdi = 'Ali'
+musteriSoyadi = 'Yılmaz'
+musteriAdSoyad = musteriAdi + ' ' + musteriSoyadi
+musteriCinsiyet = True #erkek
+musteriTcKimlik = '23221245282'
+musteriDogumYili = 1989
+musteriAdresi = 'İstanbul Kadıköy'
+musteriYasi = 2022 - musteriDogumYili
+
+
+
+# 2- Aaşağıdaki siparişlerin toplam bilgisini hesaplayınız
+    # Sipariş 1 => 100 TL
+    # Sipariş 2 => 1100.5 TL
+    # Sipariş 3 = >
+   
+
+order1 = 110 
+order2 = 1100.5
+order3 = 356.95
+total = order1 + order2 + order3
+print(total)
