b693e54e7a3c526756ddfd594969a2e3bb71df9d
BURAK OZDEMIR <ozdemirburak1912@gmail.com>
2022-10-27 10:10:31 +0300

    type conversion

--- type-conversion.py
+++ type-conversion.py
@@ -1,5 +1,25 @@
+'''
 x = input('1 sayı: ')
 y = input('2 sayı: ')
 
 toplam = int(x) + int(y)
 print(toplam)
+
+'''
+
+x = 5               #int
+y = 2.5             #float
+name = 'Çınar'      #string
+isOnline = True     #bool
+
+
+# type conversion
+
+#int to float 
+
+# x = float(x)
+# print(x)
+# print(type(x))
+
+#float to int
+
